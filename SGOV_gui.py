2fc10e29bfc73a7844743c329c619528dfa02812
Helllo <suriaman123@outlook.com>
2026-04-30 20:51:51 +0800

    Update and rename assignment6_gui.py to SGOV_gui.py

--- SGOV_gui.py
+++ SGOV_gui.py
@@ -11,7 +11,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class assignment6_ui(object):
+class sgov_ui(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
         Form.resize(896, 667)
